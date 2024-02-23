da56e3909be270a2c6ed8c097d374106e316489f
Nicolas Giraud <giraud.nicolas36@gmail.com>
2024-02-23 18:28:26 +0100

    Ajout des menus en haut et en bas avec les icones

--- app/src/main/java/com/example/projet_android/MainActivity.java
+++ app/src/main/java/com/example/projet_android/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.projet_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setOnItemSelectedListener(item -> {
+            item.setChecked(true);
+            Log.d("Mes logs", "clique sur " + item.getTitle());
+
+            return false;
+        });
     }
 }
